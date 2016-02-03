0e9c94362df342d9725997d74222788d863e710d
Joel Trottier-Hebert <iamquebecois@gmail.com>
2016-02-03 11:03:17 -0500

    Initial commit

---
+++ src/main/java/com/arcbees/client/gin/ClientModule.java
@@ -0,0 +1,25 @@
+package com.arcbees.client.gin;
+
+import com.arcbees.client.application.ApplicationModule;
+import com.arcbees.client.place.NameTokens;
+import com.arcbees.client.resources.ResourceLoader;
+import com.gwtplatform.mvp.client.annotations.DefaultPlace;
+import com.gwtplatform.mvp.client.annotations.ErrorPlace;
+import com.gwtplatform.mvp.client.annotations.UnauthorizedPlace;
+import com.gwtplatform.mvp.client.gin.AbstractPresenterModule;
+import com.gwtplatform.mvp.client.gin.DefaultModule;
+
+public class ClientModule extends AbstractPresenterModule {
+    @Override
+    protected void configure() {
+        install(new DefaultModule.Builder().build());
+        install(new ApplicationModule());
+
+        bind(ResourceLoader.class).asEagerSingleton();
+
+        // DefaultPlaceManager Places
+        bindConstant().annotatedWith(DefaultPlace.class).to(NameTokens.HOME);
+        bindConstant().annotatedWith(ErrorPlace.class).to(NameTokens.HOME);
+        bindConstant().annotatedWith(UnauthorizedPlace.class).to(NameTokens.HOME);
+    }
+}
